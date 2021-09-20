ca03d98f1995569eca2a538623fac5b3eb448d84
wengjiaqi <1845252982@qq.com>
2021-09-20 13:20:38 +0800

    feat: addbutton

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./components/Button/button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button btnType="primary" size="larget">
+        primary
+      </Button>
+      <Button btnType="ghost" size="small">
+        ghost
+      </Button>
+      <Button btnType="dashed" size="larget">
+        dashed
+      </Button>
+      <Button btnType="link" size="larget" shape="round">
+        link
+      </Button>
+      <Button btnType="text" size="larget">
+        text
+      </Button>
     </div>
   );
 }
